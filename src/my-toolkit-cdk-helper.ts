ec13dc798ebcc811fbd635dd8b076df4ddfd2fd8
Federico Pereyra López <Federico@FedenoMacBook-Pro.local>
2025-03-23 19:56:13 +0100

    Initial commit

---
+++ src/my-toolkit-cdk-helper.ts
@@ -0,0 +1,16 @@
+import * as path from 'path';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { MyToolkitPackage } from './bazel-package';
+
+export class MyToolkit {
+  public static fromWorkspace(pkg: MyToolkitPackage): lambda.Code {
+    // Adjust as necessary for your environment
+    const artifactPath = path.join(
+      process.cwd(),
+      pkg.packagePath,
+      'artifact.zip'
+    );
+
+    return lambda.Code.fromAsset(artifactPath);
+  }
+}
---
+++ src/my-toolkit-package.ts
@@ -0,0 +1,30 @@
+import * as path from 'path';
+
+export class MyToolkitPackage {
+    readonly packageName: string;
+    readonly packagePath: string;
+
+    constructor(packageName: string, packagePath: string) {
+        this.packageName = packageName;
+        this.packagePath = packagePath;
+    }
+
+    builder() {
+        return new MyToolkitPackageBuilder();
+    }
+}
+
+class MyToolkitPackageBuilder {
+    private packageName: string = '';
+    private packagePath: string = '';
+
+    public withName(packageName: string): MyToolkitPackageBuilder {
+        this.packageName = packageName;
+        this.packagePath = path.join(process.cwd(), 'bazel-bin', packageName);
+        return this;
+    }
+
+    public build(): MyToolkitPackage {
+        return new MyToolkitPackage(this.packageName, this.packagePath);
+    }
+}
